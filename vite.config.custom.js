a42d7fec01ccc693e0880e0c848722d17ae59d85
rfogle1322-maker <rfogle1322@gmail.com>
2026-07-15 15:09:34 -0500

    Update vite.config.custom.js

--- vite.config.custom.js
+++ vite.config.custom.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -9,9 +10,4 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  define: {
-    'base44': '({})',
-    'window.base44': '({})',
-    'process.env': {}
-  }
-});
+})
