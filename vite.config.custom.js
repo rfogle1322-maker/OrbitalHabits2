676fb2c27957e8344d9045ec0cd51457794c4a05
rfogle1322-maker <rfogle1322@gmail.com>
2026-07-15 15:35:07 -0500

    Update vite.config.custom.js

--- vite.config.custom.js
+++ vite.config.custom.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { fileURLToPath } from 'node:url'
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
